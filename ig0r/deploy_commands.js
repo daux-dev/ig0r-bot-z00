9b34af6e885b4f47585fe438940d5a1e0c467aae
daux <>
2022-12-14 21:02:29 +0100

    fix command deployment script

--- ig0r/deploy_commands.js
+++ ig0r/deploy_commands.js
@@ -1,6 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const {SlashCommandBuilder, REST , Routes} = require("discord.js");
 require("dotenv").config();
 
 const commands = [
@@ -14,21 +12,19 @@ const commands = [
     .setDescription('Anmeldung zum kommenden Event! Optional mit dibs auf Schlafplatz.')
     .addStringOption(option =>
         option.setName("dibs")
-            .setDescription("Call dibs auf wo du pennen willst!")
-            .setRequired(false)
-            .addChoice("couch", "couch")
-            .addChoice("smolcouch", "smolcouch")
-            .addChoice("bigcouch", "bigcouch")
-            .addChoice("matratze", "matratze")
-            .addChoice("feldbett1", "feldbett1")
-            .addChoice("feldbett2", "feldbett2")
-            .addChoice("feldbett3", "feldbett3")
-            ),
-	// new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+            .setDescription("Call dibs auf wo du schlafen willst!")
+            .addChoices(
+                {name: "couch", value: "couch"},
+                {name: "smolcouch", value: "smolcouch"},
+                {name: "bigcouch", value: "bigcouch"},
+                {name: "matratze", value: "matratze"},
+                {name: "feldbett1", value: "feldbett1"},
+                {name: "feldbett2", value: "feldbett2"},
+                {name: "feldbett3", value: "feldbett3"},
+            )),
+];
 
-const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
 	try {
